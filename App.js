64749284abb9de8a3f49acdaed25d77e0ec0e0ab
Alexandr <Alexandr.Florinskij@yandex.ua>
2019-07-22 17:20:03 +0300

    demo app

---
+++ App.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react'
+import { View, TouchableOpacity, Text, Platform } from 'react-native';
+import { VGSCardIOModule, CardIOUtilities } from 'react-native-awesome-card-io';
+
+
+export default function App() {
+
+  const [card, setCard] = useState([]);
+  const [loading, setLoading] = useState(false);
+  
+  const storeCard = cardItem => {
+    setCard([
+      ...card,
+      cardItem
+    ]);
+  }
+
+  useEffect(() => {
+    return () => {
+      if (Platform.OS === 'ios') {
+        CardIOUtilities.preload()
+      }
+    }
+  }, []);
+
+  const scanCardVGS = () => {
+    setLoading(true)
+    VGSCardIOModule
+      .scanCard()
+      .redactCard({
+        vaultUrl: 'https://tntxwjvjals.SANDBOX.verygoodproxy.com',
+        path: '/post',
+        method: 'POST',
+        // headers: {
+        //   'Content-Type': 'application/json',
+        // }
+      })
+      .then(card => {
+        storeCard(card)
+        setLoading(false)
+      })
+      .catch(() => {
+        // the user cancelled
+      })
+  }
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {card.map((item, key) => (
+        <Text key={key}>{item.cardNumber} - {item.expiryMonth} / {item.expiryYear}</Text>
+      ))}
+      <TouchableOpacity onPress={() => scanCardVGS()} style={{marginTop:30}}>
+        <Text>{loading ? 'Loading...' : 'Scan card!'}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
